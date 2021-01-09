21081538502d9d66b0bdf0462399ba7e003f8413
Mahesh Challapalli <maheshchallapalli@Maheshs-MBP.lan>
2021-01-09 11:43:08 -0800

    finish project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return 'hello';
+  return <div>
+    <Folder name="Desktop">
+      <Folder name="Music">
+        <File name="allstar.mp3"/>
+        <File name="one.mp3"/>
+      </Folder>
+      <File name="dog.jpeg"/>
+      <File name="cats.png"/>
+    </Folder>
+  </div>
 }
+
+const Folder = (props) => {
+  const [ isOpen, setIsOpen ] = useState(true);
+  const { name, children } = props;
+
+  const handleClick = () => setIsOpen(!isOpen);
+  
+
+  return <div>
+    <span onClick={handleClick}>{name}</span> 
+    <div style={{ marginLeft: '17px' }}>
+      {isOpen ? children : null}
+    </div>
+  </div> 
+};
+
+const File = (props) => {
+  return <div>{props.name}</div>
+};
 
 export default App;
